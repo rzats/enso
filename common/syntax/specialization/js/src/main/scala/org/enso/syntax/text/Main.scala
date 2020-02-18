7599d44241af052361bc76c1cdf7a30e9d2a3406
Josef Vonasek <j.f.vonasek@gmail.com>
2020-02-18 11:49:06 +0100

    decode id map from json

--- common/syntax/specialization/js/src/main/scala/org/enso/syntax/text/Main.scala
+++ common/syntax/specialization/js/src/main/scala/org/enso/syntax/text/Main.scala
@@ -1,15 +1,18 @@
 package org.enso.syntax.text
 
 import org.enso.flexer.Reader
+import org.enso.syntax.text
 
 import scala.scalajs.js.annotation._
 import scala.scalajs.js
+
+import io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
 
 object Parse {
     @JSExportTopLevel("parse")
     def parse(input: String): String = {
       try {
-        new Parser().run(new Reader(input)).toJson().noSpacesSortKeys
+        new text.Parser().run(new Reader(input)).toJson().noSpacesSortKeys
       } catch {
         // FIXME We wrap the error message in JavaScriptException, so that javascript
         //  can display it. This is no longer needed in scalajs 1.0
@@ -20,8 +23,10 @@ object Parse {
     @JSExportTopLevel("parseWithIDs")
     def parse(input: String, idsJSON: String): String = {
       try {
-        val ids: Parser.IDMap = ??? // TODO [Josef] decode from JSON
-        new Parser().run(new Reader(input), ids).toJson().noSpacesSortKeys
+        val ids = decode[Parser.IDMap](idsJSON).getOrElse {
+          throw new Exception("Could not decode IDMap from json.")
+        }
+        new text.Parser().run(new Reader(input), ids).toJson().noSpacesSortKeys
       } catch {
         // FIXME We wrap the error message in JavaScriptException, so that javascript
         //  can display it. This is no longer needed in scalajs 1.0
