d64d1c405128733e91fb657778f9698bd2b1f984
Josef Vonasek <j.f.vonasek@gmail.com>
2020-02-18 15:58:45 +0100

    pass span as tuple

--- common/syntax/specialization/js/src/main/scala/org/enso/syntax/text/Main.scala
+++ common/syntax/specialization/js/src/main/scala/org/enso/syntax/text/Main.scala
@@ -5,32 +5,25 @@ import org.enso.syntax.text
 
 import scala.scalajs.js.annotation._
 import scala.scalajs.js
-
-import io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+import io.circe.parser._
+import org.enso.data.{Index, Size, Span}
 
 object Parse {
-    @JSExportTopLevel("parse")
-    def parse(input: String): String = {
-      try {
-        new text.Parser().run(new Reader(input)).toJson().noSpacesSortKeys
-      } catch {
-        // FIXME We wrap the error message in JavaScriptException, so that javascript
-        //  can display it. This is no longer needed in scalajs 1.0
-        case e : Throwable => throw js.JavaScriptException(e.getMessage)
-      }
-    }
-
-    @JSExportTopLevel("parseWithIDs")
-    def parse(input: String, idsJSON: String): String = {
-      try {
-        val ids = decode[Parser.IDMap](idsJSON).getOrElse {
+  @JSExportTopLevel("parse")
+  def parse(input: String, idsJSON: String): String = {
+    try {
+      val ids = decode[Seq[((Int, Int), AST.ID)]](idsJSON)
+        .getOrElse {
           throw new Exception("Could not decode IDMap from json.")
         }
-        new text.Parser().run(new Reader(input), ids).toJson().noSpacesSortKeys
-      } catch {
-        // FIXME We wrap the error message in JavaScriptException, so that javascript
-        //  can display it. This is no longer needed in scalajs 1.0
-        case e : Throwable => throw js.JavaScriptException(e.getMessage)
-      }
+        .map {
+          case ((ix, len), id) => (Span(Index(ix), Size(len)), id)
+        }
+      new text.Parser().run(new Reader(input), ids).toJson().noSpacesSortKeys
+    } catch {
+      // FIXME We wrap the error message in JavaScriptException, so that javascript
+      //  can display it. This is no longer needed in scalajs 1.0
+      case e: Throwable => throw js.JavaScriptException(e.getMessage)
     }
+  }
 }
