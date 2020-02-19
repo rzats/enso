0c953d5a94bb5d6b255fc9161062608ffc7fbeec
Josef Vonasek <j.f.vonasek@gmail.com>
2020-02-19 10:51:59 +0100

    update parser service

--- common/syntax/specialization/js/src/main/scala/org/enso/syntax/text/Main.scala
+++ common/syntax/specialization/js/src/main/scala/org/enso/syntax/text/Main.scala
@@ -3,23 +3,19 @@ package org.enso.syntax.text
 import org.enso.flexer.Reader
 import org.enso.syntax.text
 
+import io.circe.parser._
 import scala.scalajs.js.annotation._
 import scala.scalajs.js
-import io.circe.parser._
-import org.enso.data.{Index, Size, Span}
 
 object Parse {
   @JSExportTopLevel("parse")
-  def parse(input: String, idsJSON: String): String = {
+  def parse(program: String, idsJson: String): String = {
     try {
-      val ids = decode[Seq[((Int, Int), AST.ID)]](idsJSON)
-        .getOrElse {
-          throw new Exception("Could not decode IDMap from json.")
-        }
-        .map {
-          case ((ix, len), id) => (Span(Index(ix), Size(len)), id)
-        }
-      new text.Parser().run(new Reader(input), ids).toJson().noSpacesSortKeys
+      val ids = decode(idsJson).getOrElse {
+        throw new Exception("Could not decode IDMap from json.")
+      }
+      val ast = new text.Parser().run(new Reader(program), text.Parser.idMap(ids))
+      ast.toJson().noSpacesSortKeys
     } catch {
       // FIXME We wrap the error message in JavaScriptException, so that javascript
       //  can display it. This is no longer needed in scalajs 1.0
